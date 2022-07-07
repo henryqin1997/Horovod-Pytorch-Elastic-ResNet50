56e273d5ba94d057d663d7ddca221bc907788b13
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2022-07-06 17:29:55 -0700

    For running

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 horovodrun -np 8 --min-np 4 --max-np 8 --host-discovery-script discover_hosts.sh \
 python3 train_resnet_imagenet.py \
 --train-dir /workspace/data/ILSVRC2012/train \
---val-dir /workspace/data/ILSVRC2012/validation \
+--val-dir /workspace/data/ILSVRC2012/validation
 
